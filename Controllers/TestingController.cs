5ee32e71157ab0dc18509288348fd63b8b881233
yazankassam.codavia <yazankassam.codavia@gmail.com>
2021-05-16 14:06:34 +0300

    initial proj

---
+++ Controllers/TestingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iread_api_gateway_ms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestingController : ControllerBase
+    {
+         // GET: Testing/api/msg
+        [HttpGet]
+        public async Task<IActionResult> GetMsg()
+        {
+            return Ok($"Hellow from api gateway");
+        }
+    }
+}
